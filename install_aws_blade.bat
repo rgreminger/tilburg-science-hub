95b63280c244b17d1756af32dac7af34752a9ee5
Hannes Datta <h.datta@tilburguniversity.edu>
2020-04-13 13:22:34 +0200

    add installation script for blade AWS
    warning: overwrites user variables; to be fixed.

---
+++ install_aws_blade.bat
@@ -0,0 +1,5 @@
+"c:\python27\scripts\pip.exe" install --upgrade --user awscli
+c:\Windows\system32\setx PATH %USERPROFILE%\AppData\Roaming\Python\Scripts\;c:\python27\
+"%USERPROFILE%\AppData\Roaming\Python\Scripts\aws.cmd" configure
+
+
